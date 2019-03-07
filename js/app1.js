9874e4d12e5115fe3a348f6d5ccb2f4d658f68b6
rezmuslia@gmail.com <60576057r>
2019-03-07 19:49:17 +0300

    two images

--- js/app1.js
+++ js/app1.js
@@ -1,32 +1,42 @@
 // const itemEl = window.item;  // элементы с id становятся свойствами глобального объекта (но так лучше не делать0
 // console.log(itemEl);
-
-const imageEl = document.createElement('div');
-imageEl.id = 'image';
-imageEl.draggable = true;
+const im1= 'https://im0-tub-ru.yandex.net/i?id=844dc629d8aa8584f2de091cd5f002a5-l&n=13';
+const im2= 'https://im0-tub-ru.yandex.net/i?id=6b5203601a8a69d5f8894cdb32abd3ab-l&n=13';
 
 
-imageEl.src = 'https://im0-tub-ru.yandex.net/i?id=844dc629d8aa8584f2de091cd5f002a5-l&n=13';
+const imageEl2 = document.createElement('img');
+list = [];
+list.push(im1,im2)
+
+
+
+for (const im of list) {
+    document.body.appendChild(drag(im));
+}
+
+
+
 let draggable = null;
 
-imageEl.addEventListener('dragstart', (event) => {
-event.dataTransfer();
-draggable = event.currentTarget;
-console.log('dragstart');
+function drag(img){
+    const imageEl = document.createElement('img');
+    imageEl.draggable = true;
+    imageEl.src =  img;
+    imageEl.width = '100';
+    imageEl.height = '100';
 
-//itemEl.addEventListener('dragend', (event) => {
-//    draggable = null;
-//    console.log('dragend');
-//});
+imageEl.addEventListener('dragstart', (event) => {
+    draggable = event.currentTarget;
+});
+
+imageEl.addEventListener('dragend', (event) => {
+    draggable = null;
+    console.log('dragend');
+});
+return imageEl;}
 
 const moveAreaEl = document.querySelector('#move-area');
 const copyAreaEl = document.querySelector('#copy-area');
-
-if (localStorage.getItem('position') === 'move') {
-    moveAreaEl.appendChild(imageEl);
-} else {
-    document.body.appendChild(imageEl);
-}
 
 moveAreaEl.addEventListener('dragenter', (event) => {
     console.log('dragenter');
@@ -94,3 +104,10 @@ copyAreaEl.addEventListener('drop', (event) => {
         copyAreaEl.insertBefore(clone, event.currentTarget.firstElementChild);
     }
 });
+
+
+
+
+
+
+
